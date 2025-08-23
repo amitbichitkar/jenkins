6dd9a93b41e98bcab61b646fa06cd67fabf2bd3f
amitbichitkar <root@ip-172-31-22-6.ec2.internal>
2025-08-23 14:07:33 +0000

    chage code from hello to calculator

--- Hello.java
+++ Hello.java
@@ -1,7 +1,67 @@
-     public class HelloWorld {
+import java.util.Scanner;
+
+class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("----- Simple Calculator -----");
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose operation: +  -  *  / ");
+        char op = sc.next().charAt(0);
+
+        double result;
+
+        switch (op) {
+            case '+':
+      import java.util.Scanner;
+
+class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("----- Simple Calculator -----");
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose operation: +  -  *  / ");
+        char op = sc.next().charAt(0);
+
+        double result;
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else {
+                    System.out.println("Error: Division by zero!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
     }
 }
+
 
 
